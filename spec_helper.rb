5b16b99d24e8e65ff6096e081ec261cba1078f7e
Matt Semar <matt.semar@powered.com>
2009-12-23 13:10:21 -0600

    added mac test

--- spec_helper.rb
+++ spec_helper.rb
@@ -54,7 +54,12 @@ Spec::Runner.configure do |config|
       @selenium.start
       @selenium.open
     else
-      @selenium = Selenium::SeleniumDriver.new("localhost", 4444, "*chrome", @url, 60)
+      sel_type = "*chrome"
+      if `hostname` =~ /Mac/i
+        puts "changing selenium driver type"
+        sel_type =  "*firefox3"
+      end
+      @selenium = Selenium::SeleniumDriver.new("localhost", 4444, sel_type, @url, 60)
       @selenium.start
     end
     @selenium.open(@url)
